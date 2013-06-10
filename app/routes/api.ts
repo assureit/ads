7739b5d882824776d247f06be24df81ef8bc4232
makino <makino@arte-inc.co.jp>
2013-06-10 18:17:02 +0900

    refactoring

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -7,8 +7,5 @@ jsonrpc.add('version', function(params: any, callback: type.Callback) {
 });
 
 jsonrpc.addModule(dcase);
-// for (var fn in dcase) {
-// 	if (typeof dcase[fn] === 'function') jsonrpc.add(fn, dcase[fn]);
-// }
 
 export var httpHandler = jsonrpc.httpHandler;
