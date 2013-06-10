31ecee989bd99a4dac965cbfdb133a9136765e91
makino <makino@arte-inc.co.jp>
2013-06-10 14:51:37 +0900

    added database files

---
+++ db/create_user_test.sql
@@ -0,0 +1,9 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+CREATE SCHEMA IF NOT EXISTS `ads` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `ads` ;
+
+grant all privileges on *.* to ads_test identified by 'ads_test';
+grant all privileges on *.* to ads_test@'localhost' identified by 'ads_test';
