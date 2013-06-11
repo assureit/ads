e327fb87a9b0d05b4eeedae3d542c81181e684ba
uchida <uchida.atsushi.8@gmail.com>
2013-06-11 18:59:38 +0900

    fix client API getDCaseList

--- app/public/javascripts/ase.js
+++ app/public/javascripts/ase.js
@@ -76,7 +76,7 @@ var ASE = function(body) {
 			var id = dcase.dcaseId;
 			var name = dcase.dcaseName;
 			var user = dcase.userName;
-			var lastDate = new DateFormatter(dcase.latestCommit.time).format();
+			var lastDate = dcase.latestCommit.dateTime;//new DateFormatter(dcase.latestCommit.time).format();
 			var lastUser = dcase.latestCommit.userName;
 			var html = "<td><a href=\"./?dcaseId=" + id + "\">" + name + 
 					"</a></td><td>" + user + "</td><td>" + lastDate + "</td><td>" +
