859eade6c3002686fb42def06a6ff7ce9690af8c
makino <makino@arte-inc.co.jp>
2013-06-18 11:27:40 +0900

    fixed declaration

--- app/types/mysql.d.ts
+++ app/types/mysql.d.ts
@@ -1,11 +1,14 @@
+///<reference path='../DefinitelyTyped/node/node.d.ts'/>
 declare module 'mysql' {
+    import events = module('events')
     declare function createConnection(config: any): Connection;
     declare function createPool(config: any);
 
     interface QueryCallback {
         (err:any, result:any) : void;
     }
-    class Connection {
+    class Connection extends events.EventEmitter {
+    // class Connection implements EventEmitter {
         static createQuery(sql, values?, cb?): any;
         connect(cb?): void;
         changeUser(options, cb): any;
@@ -18,5 +21,15 @@ declare module 'mysql' {
         resume(): void;
         escape(value): string;
         format(sql, values): string;
+
+        // // EventEmitter Interface
+        // addListener(event: string, listener: Function);
+        // on(event: string, listener: Function);
+        // once(event: string, listener: Function): void;
+        // removeListener(event: string, listener: Function): void;
+        // removeAllListener(event: string): void;
+        // setMaxListeners(n: number): void;
+        // listeners(event: string):  {Function;}[];
+        // emit(event: string, arg1?: any, arg2?: any): void;
     } 
 }
