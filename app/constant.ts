44f8cce4384cdd00f8f74d4ebd88b39018c40c78
makino <makino@arte-inc.co.jp>
2013-06-24 19:38:25 +0900

    merged

--- app/constant.ts
+++ app/constant.ts
@@ -1,3 +1,3 @@
 export var SYSTEM_USER_ID = 1;
-export var UPLOAD_DIR = 'upload';export var SYSTEM_USER_ID = 1;
+export var UPLOAD_DIR = 'upload';
 export var basepath  = "";
--- app/test/routes/file.js
+++ app/test/routes/file.js
@@ -88,7 +88,6 @@ describe('api', function () {
         });
         it('should return name and fileBody', function (done) {
             request(app['app']).get('/file/110').expect(200).end(function (err, res) {
-                console.log(res);
                 assert.equal(res.header['content-type'], 'application/octet-stream');
                 assert.equal(res.header['content-disposition'], 'attachment; filename="uptest.txt"');
                 assert.equal(res.text, 'アップロードテスト用のファイルです\n');
