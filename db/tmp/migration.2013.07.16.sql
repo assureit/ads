3342f79bb1a06cc9b98e593f2ca3bd48f9d8534b
makino <makino@arte-inc.co.jp>
2013-07-16 17:02:46 +0900

    modified migration sql fit to ads/ads_test.

--- db/tmp/migration.2013.07.16.sql
+++ db/tmp/migration.2013.07.16.sql
@@ -2,9 +2,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-ALTER TABLE `ads`.`monitor_node` ADD COLUMN `delete_flag` TINYINT(1) NOT NULL DEFAULT 0  AFTER `publish_status` , ADD COLUMN `created` DATETIME NULL DEFAULT NULL  AFTER `delete_flag` , ADD COLUMN `modified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  AFTER `created` ;
+ALTER TABLE `monitor_node` ADD COLUMN `delete_flag` TINYINT(1) NOT NULL DEFAULT 0  AFTER `publish_status` , ADD COLUMN `created` DATETIME NULL DEFAULT NULL  AFTER `delete_flag` , ADD COLUMN `modified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  AFTER `created` ;
 
-CREATE  TABLE IF NOT EXISTS `ads`.`tag` (
+CREATE  TABLE IF NOT EXISTS `tag` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `label` VARCHAR(1024) NOT NULL ,
   `created` DATETIME NULL DEFAULT NULL ,
@@ -14,7 +14,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
-CREATE  TABLE IF NOT EXISTS `ads`.`dcase_tag_rel` (
+CREATE  TABLE IF NOT EXISTS `dcase_tag_rel` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `dcase_id` INT(11) NOT NULL ,
   `tag_id` INT(11) NOT NULL ,
@@ -25,19 +25,19 @@ CREATE  TABLE IF NOT EXISTS `ads`.`dcase_tag_rel` (
   INDEX `fk_dcase_tag_rel_tag1` (`tag_id` ASC) ,
   CONSTRAINT `fk_dcase_tag_rel_dcase1`
     FOREIGN KEY (`dcase_id` )
-    REFERENCES `ads`.`dcase` (`id` )
+    REFERENCES `dcase` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_dcase_tag_rel_tag1`
     FOREIGN KEY (`tag_id` )
-    REFERENCES `ads`.`tag` (`id` )
+    REFERENCES `tag` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
-DROP TABLE IF EXISTS `ads`.`node_property` ;
+DROP TABLE IF EXISTS `node_property` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
