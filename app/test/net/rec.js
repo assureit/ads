1ec3eac463fe66b9a9881ee41c7058ba94720bcd
terao <terao@arte-inc.co.jp>
2013-07-26 15:51:14 +0900

    unittest update 08

--- app/test/net/rec.js
+++ app/test/net/rec.js
@@ -7,9 +7,12 @@ var CONFIG = require('config');
 var app = express();
 
 var responseFlag = true;
+var recRequestBody;
+
 app.use(express.bodyParser());
 app.post('/rec/api/1.0', function (req, res) {
     res.header('Content-Type', 'application/json');
+    recRequestBody = req.body;
     if (responseFlag) {
         res.send(JSON.stringify({ jsonrpc: "2.0", result: null, id: 1 }));
     } else {
@@ -49,6 +52,23 @@ describe('net', function () {
                     expect(err.rpcHttpStatus).not.to.be(null);
                     expect(err.rpcHttpStatus).not.to.be(undefined);
                     expect(err.rpcHttpStatus).to.eql(500);
+                    done();
+                });
+            });
+            it('request parameter check', function (done) {
+                responseFlag = true;
+                recRequestBody = null;
+                var rc = new rec.Rec();
+                rc.request('test_method', { check: 'test' }, function (err, result) {
+                    expect(err).to.be(null);
+                    expect(result).not.to.be(null);
+                    expect(result).not.to.be(undefined);
+                    expect(result.jsonrpc).to.eql("2.0");
+                    expect(result.result).to.be(null);
+                    expect(result.id).to.eql(1);
+                    expect(recRequestBody).not.to.be(null);
+                    expect(recRequestBody.method).to.be('test_method');
+                    expect(recRequestBody.params.check).to.be('test');
                     done();
                 });
             });
