c800534091144c9eef334cf4fce1211ae311991f
terao <terao@arte-inc.co.jp>
2013-07-26 15:24:38 +0900

    unittest update 06

--- app/test/model/issue.ts
+++ app/test/model/issue.ts
@@ -8,11 +8,33 @@ import error = module('../../api/error')
 import testdata = module('../testdata')
 var expect = require('expect.js');	// TODO: import module化
 var async = require('async')
+var CONFIG = require('config')
+var express = require('express');
+
+var redmineRequestBody:any;
+var app = express();
+app.use(express.bodyParser());
+app.post('/issues.json', function (req: any, res: any) {
+	res.header('Content-Type', 'application/json');
+	redmineRequestBody = req.body;
+	res.send(JSON.stringify({"issue":{"id":3825}}));
+});
 
 describe('model', function() {
 	var testDB;
 	var con: db.Database
 	var issueDAO: model_issue.IssueDAO;
+	var server = null;
+
+	before((done) => {
+		CONFIG.redmine.port = 3030;
+		server = app.listen(3030).on('listening', done);
+	});
+	after(() => {
+		server.close();
+		CONFIG.redmine.port = CONFIG.getOriginalConfig().redmine.port;
+		CONFIG.resetRuntime((err, written, buffer) => {});
+	});
 
 	beforeEach(function (done) {
 		testdata.begin(['test/default-data.yaml', 'test/model/issue.yaml'], (err:any, c:db.Database) => {
@@ -86,6 +108,20 @@ describe('model', function() {
 				issueDAO.publish(201, (err:any) => {
 					expect(err).to.be(null);
 					con.query('SELECT * FROM issue WHERE dcase_id=201 AND its_id is null', (err, resultIssue) => {
+						expect(err).to.be(null);	
+						expect(resultIssue).not.to.be(null);
+						expect(resultIssue.length).to.eql(0);
+						done();
+					});
+				});
+			});
+			it('redmine parameter check', function(done) {
+				issueDAO.publish(202, (err:any) => {
+					expect(err).to.be(null);
+					expect(redmineRequestBody).not.to.be(null);
+					expect(redmineRequestBody.issue.subject).to.eql('test data04');
+					expect(redmineRequestBody.issue.description).to.eql('test description04');
+					con.query('SELECT * FROM issue WHERE dcase_id=202 AND its_id is null', (err, resultIssue) => {
 						expect(err).to.be(null);	
 						expect(resultIssue).not.to.be(null);
 						expect(resultIssue.length).to.eql(0);
