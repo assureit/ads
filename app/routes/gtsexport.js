dfa0e7895278e77b1c3b54f0e9a17f2d24701393
uchida <uchida.atsushi.8@gmail.com>
2013-08-23 00:15:26 +0900

    fix GTS Export

--- app/routes/gtsexport.js
+++ app/routes/gtsexport.js
@@ -12,6 +12,10 @@ var db = require('../db/db');
 var async = require('async');
 var _ = require('underscore');
 
+function getMethodName(node) {
+    return node.NodeType.slice(0, 1) + node.ThisNodeId;
+}
+
 function exporter(req, res) {
     var con = new db.Database();
 
@@ -24,11 +28,35 @@ function exporter(req, res) {
         con.close();
         var c = result[0].c;
         var d = result[0].d;
-        res.send({
+
+        var json = {
             commitId: c.id,
             dcaseName: d.name,
-            contents: c.data
-        });
+            contents: JSON.parse(c.data)
+        };
+
+        var resText = "";
+        console.log(json.contents.NodeList);
+        for (var i = 0; i < json.contents.NodeList.length; i++) {
+            var node = json.contents.NodeList[i];
+            if (node.MetaData == null) {
+                continue;
+            }
+            for (var j = 0; j < node.MetaData.length; j++) {
+                var data = node.MetaData[j];
+                console.log(data);
+                if (data.Description == null) {
+                    continue;
+                }
+                if (data.Type == "Monitor" || data.Type == "Recovery" || data.Type == "Condition") {
+                    resText += '// ' + node.ThisNodeId + '\n';
+                    resText += "void " + getMethodName(node) + '_' + data.Type + '() {\n';
+                    resText += '    ' + data.Description.replace(/\n/g, '\n    ');
+                    resText += '\n}\n\n';
+                }
+            }
+        }
+        res.send(resText);
     });
 }
 exports.exporter = exporter;
--- app/routes/gtsexport.ts
+++ app/routes/gtsexport.ts
@@ -14,6 +14,10 @@ import error = module('../api/error')
 var async = require('async')
 var _ = require('underscore');
 
+function getMethodName(node: any): string {
+	return node.NodeType.slice(0,1) + node.ThisNodeId;
+}
+
 export function exporter(req: any, res: any) {
 	//res.send("id:"+ req.params.id + ", type:"+ req.params.type + ", n:"+ req.params.n);
 	var con = new db.Database();
@@ -27,10 +31,35 @@ export function exporter(req: any, res: any) {
 		con.close();
 		var c = result[0].c;
 		var d = result[0].d;
-		res.send({
+
+		var json = {
 			commitId: c.id,
 			dcaseName: d.name,
-			contents: c.data
-		});
+			contents: JSON.parse(c.data)
+		};
+
+		var resText :string = "";
+		console.log(json.contents.NodeList);
+		for(var i:number = 0; i < json.contents.NodeList.length; i++) {
+			var node:any = json.contents.NodeList[i];
+			if(node.MetaData == null) {
+				continue;
+			}
+			for(var j:number = 0; j < node.MetaData.length; j++) {
+				var data = node.MetaData[j];
+				console.log(data);
+				if(data.Description == null) {
+					continue;
+				}
+				if(data.Type == "Monitor" || data.Type == "Recovery" || data.Type == "Condition") {
+					resText += '// ' + node.ThisNodeId + '\n';
+					resText += "void " + getMethodName(node) + '_' + data.Type+ '() {\n';
+					resText += '    ' + data.Description.replace(/\n/g, '\n    ');
+					resText += '\n}\n\n';
+				}
+			}
+		}
+		res.send(resText);
 	});
 }
+
