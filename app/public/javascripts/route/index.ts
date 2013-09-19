3905f18bd1bcaab012ebfafc4a5afafbef05b646
uchida <uchida.atsushi.8@gmail.com>
2013-09-19 10:14:58 +0900

    add importXML to index.ts

--- app/public/javascripts/route/index.ts
+++ app/public/javascripts/route/index.ts
@@ -1,5 +1,33 @@
 ///<reference path='../../../DefinitelyTyped/jquery/jquery.d.ts'/>
+///<reference path='../Xml2DCaseTree.ts'/>
+///<reference path='../DCaseTree.ts'/>
 
+//TODO resolve dupulicate library
+//<reference path='../../assurejs/src/CaseEncoder.ts'/>
+//<reference path='../../assurejs/src/Converter.ts'/>
+//<reference path='../../assurejs/src/CaseDecoder.ts'/>
+//<reference path='../../assurejs/src/CaseModel.ts'/>
+//<reference path='../../assurejs/src/PlugInManager.ts'/>
+
+declare module AssureIt {
+	export class Converter {
+		GenNewJson(s:any):any;
+	}
+	export class CaseEncoder {
+		ConvertToASN(a:any, b:any):any;
+	}
+	export class CaseDecoder {
+		ParseJson(Case:Case, s:any):any;
+	}
+	export class Case {
+		constructor(name:string, a:number, b:number, PlugInManager: PlugInManager);
+		SetElementTop(root:any);
+		ElementTop: any;
+	}
+	export class PlugInManager {
+		constructor(path: string);
+	}
+}
 /*
 $(()=>{
 	var ads: ADS = new ADS(document.getElementById("ase"));
@@ -53,4 +81,33 @@ $(()=>{
 			}
 		}
 	});
+
+	var importFile = new ImportFile(".panel");
+	importFile.read((file, target)=> {
+		var x2dc = new Xml2DCaseTree.Converter();
+		var tree = x2dc.parseXml(file.result);
+		var j = tree.convertAllChildNodeIntoJson([]);
+
+		var converter = new AssureIt.Converter();
+		var encoder = new AssureIt.CaseEncoder();
+		var decoder = new AssureIt.CaseDecoder();
+
+		var s:any = {};
+		s.contents = JSON.stringify({
+			DCaseName: file.name,
+			NodeCount: tree.NodeCount,
+			TopGoalId: tree.TopGoalId,
+			NodeList: j
+		});
+
+		var JsonData = converter.GenNewJson(s);
+		var Case0 = new AssureIt.Case(file.name, 0, 0, new AssureIt.PlugInManager("FIXME"));
+		var root = decoder.ParseJson(Case0, JsonData);
+		Case0.SetElementTop(root);
+		var encoded = encoder.ConvertToASN(Case0.ElementTop, false);
+		var projectId = parseInt($(target).attr("id").replace(/[a-zA-Z]*/, ""));
+		var r = DCaseAPI.createDCase(file.name, encoded, projectId);
+		location.href = "./case/" + r.dcaseId;
+	});
+
 });
