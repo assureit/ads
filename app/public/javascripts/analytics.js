b620e93b2a986e9409015663d323ab3c9f55a18c
Ubuntu <ubuntu@ip-10-0-0-196.eu-west-1.compute.internal>
2013-09-24 23:07:23 +0000

    Updated analytics.js

--- app/public/javascripts/analytics.js
+++ app/public/javascripts/analytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-26392110-2', 'ynu.ac.jp');
+  ga('create', 'UA-26392110-2', location.hostname);
   ga('send', 'pageview');
