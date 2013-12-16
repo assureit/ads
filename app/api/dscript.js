2188500af4f587f11e5cb634802564d9854ec537
takeo.onoda <takeo.onoda@gmail.com>
2013-12-17 07:32:05 +0900

    add ExecDScript as ServerApi

--- app/api/dscript.js
+++ app/api/dscript.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var child_process = require('child_process');
 
 var error = require('./error');
 var CONFIG = require('config');
@@ -43,4 +44,89 @@ function getDScript(params, userId, callback) {
     }
 }
 exports.getDScript = getDScript;
+
+function ensureDir(path) {
+    try  {
+        fs.statSync(path);
+    } catch (e) {
+        fs.mkdirSync(path);
+    }
+}
+var status = {
+    DScriptRootDir: "/tmp/dscript-v2/",
+    DScriptExecDir: "/tmp/dscript-v2/" + process.pid + "/",
+    Child: null
+};
+
+function execDScriptOnGreenTea(script) {
+    var execFile = status.DScriptExecDir + "dscript.ds";
+    fs.writeFileSync(execFile, script);
+    if (status.Child != null) {
+        console.log("DScript: kill child process which starts before");
+        (status.Child).kill();
+    }
+    var command = "greentea " + execFile;
+    console.log("DScript: start new child process \"" + command + "\"");
+    var child = child_process.exec(command, null, function (error, stdout, stderr) {
+    });
+    child.stdout.on('data', function (chunk) {
+        console.log(chunk);
+    });
+    child.stderr.on('data', function (chunk) {
+        console.log(chunk);
+    });
+    status.Child = child;
+}
+function execDScriptOnErlang(script) {
+    var execFile = status.DScriptExecDir + "dscript.erl";
+    fs.writeFileSync(execFile, script);
+    if (status.Child != null) {
+        console.log("DScript: kill child process which starts before");
+        (status.Child).kill();
+    }
+
+    var compile_command = "erlc -o " + status.DScriptExecDir + " " + execFile;
+    var execute_command = "erl -pa " + status.DScriptExecDir + " -noshell -run dscript main -run -init stop";
+    var command = compile_command + " && " + execute_command;
+    console.log("DScript: start new child process \"" + command + "\"");
+    var child = child_process.exec(command, null, function (error, stdout, stderr) {
+    });
+    child.stdout.on('data', function (chunk) {
+        console.log(chunk);
+    });
+    child.stderr.on('data', function (chunk) {
+        console.log(chunk);
+    });
+    status.Child = child;
+}
+function execDScript(params, userId, callback) {
+    function validate(params) {
+        var checks = [];
+        if (!params)
+            checks.push('Parameter is required.');
+        if (params && !params.lang)
+            checks.push('Langage is required.');
+        if (params && !params.script)
+            checks.push('Script is required.');
+        if (checks.length > 0) {
+            callback.onFailure(new error.InvalidParamsError(checks, null));
+        }
+        return true;
+    }
+    if (!validate(params))
+        return;
+
+    ensureDir(status.DScriptRootDir);
+    ensureDir(status.DScriptExecDir);
+    if (params.lang == "greentea") {
+        execDScriptOnGreenTea(params.script);
+        callback.onSuccess({});
+    } else if (params.lang == "erlang") {
+        execDScriptOnErlang(params.script);
+        callback.onSuccess({});
+    } else {
+        callback.onFailure(new error.InvalidParamsError(['invalid Language.'], null));
+    }
+}
+exports.execDScript = execDScript;
 
--- app/api/dscript.ts
+++ app/api/dscript.ts
@@ -1,11 +1,10 @@
 import fs = module('fs');
+import child_process = module('child_process');
 import type = module('./type')
 import error = module('./error');
 var CONFIG = require('config');
 
-
 var dscriptLibrary: { [index: string]: string } = {};
-
 
 function initDScriptLibrary(libraryPath: string) {
 	var dscriptFileNames: string[] = fs.readdirSync(libraryPath);
@@ -22,7 +21,6 @@ function initDScriptLibrary(libraryPath: string) {
 if(CONFIG.dscript.libraryPath != "") {
 	initDScriptLibrary(CONFIG.dscript.libraryPath);
 }
-
 
 export function getDScript(params: any, userId: number, callback: type.Callback) {
 	function validate(params: any): boolean {
@@ -41,5 +39,92 @@ export function getDScript(params: any, userId: number, callback: type.Callback)
 	}
 	else {
 		callback.onFailure(new error.NotFoundError('Effective DScript does not exist'));
+	}
+}
+
+/**************************************************************************************/
+
+function ensureDir(path: string) {
+	try {
+		fs.statSync(path);
+	}
+	catch(e) {
+		fs.mkdirSync(path);
+	}
+}
+var status = {
+	DScriptRootDir : "/tmp/dscript-v2/",
+	DScriptExecDir : "/tmp/dscript-v2/" + process.pid + "/",
+	Child : null,
+};
+
+function execDScriptOnGreenTea(script: string) {
+	var execFile: string = status.DScriptExecDir + "dscript.ds";
+	fs.writeFileSync(execFile, script);
+	if (status.Child != null) {
+		console.log("DScript: kill child process which starts before");
+		(<any>status.Child).kill();
+	}
+	var command = "greentea " + execFile;
+	console.log("DScript: start new child process \"" + command + "\"");
+	var child = child_process.exec(command, null, function(error, stdout, stderr) {
+		// do nothing
+	});
+	child.stdout.on('data', function(chunk: string) {
+		console.log(chunk);   // for debug
+	});
+	child.stderr.on('data', function(chunk: string) {
+		console.log(chunk);   // for debug
+	});
+	status.Child = child;
+}
+function execDScriptOnErlang(script: string) {
+	var execFile: string = status.DScriptExecDir + "dscript.erl";
+	fs.writeFileSync(execFile, script);
+	if (status.Child != null) {
+		console.log("DScript: kill child process which starts before");
+		(<any>status.Child).kill();
+	}
+
+	var compile_command = "erlc -o " + status.DScriptExecDir + " " + execFile;
+	var execute_command = "erl -pa " + status.DScriptExecDir + " -noshell -run dscript main -run -init stop";
+	var command = compile_command + " && " + execute_command;
+	console.log("DScript: start new child process \"" + command + "\"");
+	var child = child_process.exec(command, null, function(error, stdout, stderr) {
+		// do nothing
+	});
+	child.stdout.on('data', function(chunk: string) {
+		console.log(chunk);   // for debug
+	});
+	child.stderr.on('data', function(chunk: string) {
+		console.log(chunk);   // for debug
+	});
+	status.Child = child;
+}
+export function execDScript(params: any, userId: number, callback: type.Callback) {
+	function validate(params: any): boolean {
+		var checks = [];
+		if (!params) checks.push('Parameter is required.');
+		if (params && !params.lang) checks.push('Langage is required.');
+		if (params && !params.script) checks.push('Script is required.');
+		if (checks.length > 0) {
+			callback.onFailure(new error.InvalidParamsError(checks, null));
+		}
+		return true;
+	}
+	if (!validate(params)) return;
+
+	ensureDir(status.DScriptRootDir);
+	ensureDir(status.DScriptExecDir);
+	if (params.lang == "greentea") {
+		execDScriptOnGreenTea(params.script);
+		callback.onSuccess({});
+	}
+	else if (params.lang == "erlang") {
+		execDScriptOnErlang(params.script);
+		callback.onSuccess({});
+	}
+	else {
+		callback.onFailure(new error.InvalidParamsError(['invalid Language.'], null));
 	}
 }
